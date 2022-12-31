08925d9359ef82c59c3dc3cb558ee202f3b45723
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-12-31 09:20:24 +0700

    Add files via upload

---
+++ Percobaan7/Tujuh.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Restu
+ */
+public class Tujuh {
+    public static void main(String[] args) {
+        try {
+            throw new Exception("Here's my Exception");
+        }
+        catch(Exception e) {
+            System.out.println("Caught Exception");
+            System.out.println("e.getMessage():"+e.getMessage());
+            System.out.println("e.toString():"+e.toString());
+            System.out.println("e.printStackTrace():");
+            e.printStackTrace();
+        }
+    }
+}
